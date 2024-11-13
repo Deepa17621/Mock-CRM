77b1b95087d98ea6f840472ef517dd30acef5280
deepa17621 <deepasuguna2002@gmail.com>
2024-11-13 14:36:36 +0530

    create: create mail template

--- js/mail/mail.js
+++ js/mail/mail.js
@@ -20,13 +20,15 @@ async function getAllFolders() {
 getAllFolders(); // Execution Starts From Here...
 
 async function sidebar(allFolders){
-    let sideBarHtml=`<div class="sideBarContainer">`;
+    let sideBar=document.createElement("div");
+    sideBar.setAttribute("class", "sideBarContainer")
+    let sideBarHtml = `<button class="composeMailBtn" onclick="composeMail()">New Mail - Compose <i class="fa-solid fa-pen"></i> </button>`
     let foldersArray=allFolders.data;
     foldersArray.forEach(f1 => {
         sideBarHtml += `<div id="${f1.folderId}" class="btnForFolders" onclick="getListOfMail(this.id)">${f1.folderName}</div>`;
     });
-    sideBarHtml += `</div>`;
-    outerWrapper.innerHTML=sideBarHtml;
+    sideBar.innerHTML = sideBarHtml
+    outerWrapper.appendChild(sideBar);
 }
 
 // step - 3
@@ -50,12 +52,17 @@ async function getListOfMail(folderId) {
 
 // step - 5
  async function displayListOfMail(mailList) {
+
     let existingElement = document.querySelector(".aside");
+
     if(document.querySelector(".displayContainer")){
         (document.querySelector(".displayContainer")).remove();
     }
     if( existingElement ){
         existingElement.remove();
+    }
+    if(document.querySelector(".newMailContainer")){
+        (document.querySelector(".newMailContainer")).remove();
     }
     console.log(mailList);
 
@@ -78,7 +85,7 @@ async function getListOfMail(folderId) {
 
         let li = document.createElement("div");
         li.setAttribute("class", "list");
-        li.addEventListener("click", (e)=>{
+        li.addEventListener("click", (e)=>{  //step - 6
             e.preventDefault();
             if(document.querySelector(".displayContainer")){
                 (document.querySelector(".displayContainer")).remove();
@@ -97,7 +104,7 @@ async function getListOfMail(folderId) {
         wrapperForMailList.appendChild(li);
     });
  }
-
+// step - 7
  async function displayMail(folderId, messageId){
     let mailContent = await getMailContent(folderId, messageId);
     console.log(mailContent.data.content);
@@ -110,7 +117,7 @@ async function getListOfMail(folderId) {
         
     }
  }
-
+// step - 8
  async function getMailContent(folderId, messageId) {
     try {
         let response = await fetch(`/mail/displayMail/${folderId}/${messageId}`, {
@@ -126,8 +133,59 @@ async function getListOfMail(folderId) {
     }
  }
 
+ // step - 9 
+ async function composeMail() {
+    let existingElement = document.querySelector(".aside");
 
+    if(document.querySelector(".displayContainer")){
+        (document.querySelector(".displayContainer")).remove();
+    }
+    if( existingElement ){
+        existingElement.remove();
+    }
+    await getAllFolders()
+    let newMailContainer = document.querySelector("div");
+    newMailContainer.setAttribute("class", "newMailContainer");
 
+    let newMailForm = newMailFormStructure();
+
+    newMailContainer.innerHTML = newMailForm;
+    outerWrapper.appendChild(newMailContainer)
+    
+ }
+
+ function newMailFormStructure(){
+
+                return `
+                        <div class="headerForMail"><div class="sendMailBtn" title="send mail"> <i class="fa-regular fa-paper-plane" style="color: #2c66dd;"></i> Send</div></div>
+                        <form class="composeMailForm">
+                            <div class="form-Group">
+                                <label for="fromAddress">From</label>
+                                <input type="email" name="fromAddress" id="fromAddress" class="inps">
+                            </div>
+                            <div class="form-Group">
+                                <label for="toAddress">To</label>
+                                <input type="email" name="toAddress" id="toAddress" class="inps">
+                            </div>
+                            <div class="form-Group">
+                                <label for="ccAddresses">cc</label>
+                                <input type="email" name="ccAddresses" id="ccAddresses" class="inps">
+                            </div>
+                            <div class="form-Group">
+                                <label for="bccAddress">bcc</label>
+                                <input type="email" name="bccAddress" id="bccAddress" class="inps">
+                            </div>
+                            <div class="form-Group">
+                                <input type="text" name="subject" id="subject"  class="inps" placeholder="Subject">
+                            </div>
+                            <div class="middleBar">
+                                <span title="attach file here"><i class="fa-solid fa-paperclip attachFile"></i></span>-attach file
+                            </div>
+                            <div class="form-Group">
+                                <textarea name="message" id="message"  class="txtArea"></textarea>
+                            </div>
+                        </form>`
+ }
 
 
 
