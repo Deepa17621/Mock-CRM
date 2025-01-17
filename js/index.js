019ae143e918ecb8869d1158702a52bee33737b6
deepa17621 <deepasuguna2002@gmail.com>
2025-01-17 16:23:32 +0530

    added data to table-dashboard

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@
 // let code=param.get("code");
 // let loc = param.get("location");
 
+// const { random } = require("lodash");
+
+// const { get } = require("lodash");
+
 // const { get } = require("lodash");
 
 // // console.log(code);
@@ -55,10 +59,14 @@ let accountCount = document.querySelector(".account-count");
 let dealCount = document.querySelector(".deal-count");
 
 async function setCount() {
-    leadCount.textContent = await getAll("leads");
-    contactCount.textContent = await getAll("contacts");
-    accountCount.textContent = await getAll("accounts");
-    dealCount.textContent = await getAll("deals");
+    let leadData = await getAll("leads");
+    leadCount.textContent = await leadData.length;
+    let contactData = await getAll("contacts");
+    contactCount.textContent = await contactData.length;
+    let accountData = await getAll("accounts");
+    accountCount.textContent = await accountData.length;
+    let dealData = await getAll("deals");
+    dealCount.textContent = await dealData.length;
 }
 setCount();
 
@@ -70,7 +78,7 @@ async function getAll(module) {
         });
         if(res.ok){
             let data = await res.json();
-            return data.length;
+            return data;
         }
         else{
             return null;
@@ -80,3 +88,56 @@ async function getAll(module) {
     }
 }
 
+// year division-quarter performance
+let leadRow = document.querySelector(".lead-quarter-performance");
+let contactRow = document.querySelector(".contact-quarter-performance");
+let accountRow = document.querySelector(".account-quarter-performance");
+let dealRow = document.querySelector(".deal-quarter-performance");
+let dealWonRow = document.querySelector(".dealWon-quarter-performance");
+let revenueRow = document.querySelector(".revenue-quarter-performance");
+
+async function setQuarterYearPerformance() {
+    let currentDate = new Date();
+    let columnTitle = document.querySelectorAll(".col-title");
+    let q = 0;
+    console.log(columnTitle);
+    
+    columnTitle.forEach(element => {
+        if((Array.from(columnTitle).indexOf(element)) !== 0 ){
+            element.textContent = `FY ${currentDate.getFullYear()} - Q${++q}`;
+        }
+    });
+    await setEachQuarterOfEachModules(await getAll("leads"), leadRow.children);
+    await setEachQuarterOfEachModules(await getAll("contacts"), contactRow.children);
+    await setEachQuarterOfEachModules(await getAll("accounts"), accountRow.children);
+    await setEachQuarterOfEachModules(await getAll("deals"), dealRow.children);
+    
+}
+setQuarterYearPerformance();
+
+async function setEachQuarterOfEachModules(arrOfData, nodeList) {
+    let firstQuarter = 0 ;
+    let secondQuarter = 0;
+    let threeQuarter = 0;
+    let fourthQuarter = 0 ;
+    arrOfData.forEach(record => {
+        let leadDate = new Date(record.date);
+        let month = leadDate.getMonth();
+        if(([0,1,2]).includes(month)){
+            firstQuarter++;
+        }     
+        else if(([3,4,5]).includes(month)){
+            secondQuarter++;
+        }  
+        else if(([6,7,8]).includes(month)){
+            threeQuarter++;
+        }
+        else if(([9,10,11]).includes(month)){
+            fourthQuarter++;
+        }
+    });
+    nodeList[1].textContent = firstQuarter;
+    nodeList[2].textContent = secondQuarter;
+    nodeList[3].textContent = threeQuarter;
+    nodeList[4].textContent = fourthQuarter;
+}
