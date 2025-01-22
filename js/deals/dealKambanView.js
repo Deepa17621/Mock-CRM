7b6b19db8ef6df7f4dc557aba59e26c64de9a3e9
deepa17621 <deepasuguna2002@gmail.com>
2025-01-22 13:02:07 +0530

    feat: sales pipeline with stages along with drag and drop

--- js/deals/dealKambanView.js
+++ js/deals/dealKambanView.js
@@ -1,3 +1,4 @@
+
 // 1.Change View From List To Kamban
 let selectView=document.querySelector("#selectView");
 selectView.addEventListener("change", (e)=>{
@@ -51,6 +52,39 @@ async function getAllDeals(module) {
     }
 }
 
+async function getDealObjAndUpdate(id, droppedStage) {
+    try {
+        let getObjRes = await fetch(`/mongodb/getById/deals/${id}`,{
+            method:"GET",
+            headers:{"Content-Type":"application/json"}
+        });
+        if(getObjRes.ok){
+            console.log("deepa");
+            
+            let dealObj = await getObjRes.json();
+            dealObj.stage=droppedStage;
+            delete dealObj._id;
+            console.log("update");
+            
+            let updateRes = await fetch(`/mongodb/update/deals/${id}`, {
+                method:"PUT",
+                headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(dealObj)
+            });
+            if(updateRes.ok){
+                let data = await updateRes.json();
+                console.log(data);
+            }
+        }
+        else{
+            console.log("error");
+            
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 //4. Change PipeLine 
 pipeLines.addEventListener("change", async(e)=>{
     e.preventDefault();
@@ -64,12 +98,14 @@ pipeLines.addEventListener("change", async(e)=>{
             allDeals.forEach(async(element) => {    
                         if(element.pipeLine==selectedPipeLine){
                             let obj=await element;
-                            console.log(obj);
+                            // console.log(obj);
                             dealsArr.push(obj);
                         }               
             });
-            console.log(Object.values(element)[1]);
+            // console.log(Object.values(element)[1]);
             let stages=Object.values(element)[1];
+            console.log(stages);
+            
             for (const key in stages) {
                 stageContainer+=`<div id="${key}" class="multipleStagesContainer">
                                     <!-- stage Header -->
@@ -85,10 +121,64 @@ pipeLines.addEventListener("change", async(e)=>{
                                         </div>
                                     </div>
                                     <!-- stage Body -->
-                                    <div class="stageBody">${(await setDataToStageBodies(dealsArr, key))}</div>
+                                    <div class="stageBody" id="${key}">${(await setDataToStageBodies(dealsArr, key))}</div>
+                                    <div class="stage-bottom"></div>
                                 </div>`
             }
             wrapperForKanbanView.innerHTML=stageContainer;
+            const stageBody = document.querySelectorAll(".stageBody");
+            const draggableElement = document.querySelectorAll(".contentWrapper");
+            
+            for (const element of draggableElement) {
+                element.addEventListener("dragstart", (e) => {
+                    e.dataTransfer.setData("text/plain", e.target.id); // Use dataTransfer to carry element ID
+                });
+            }
+
+            // Add dragover and drop event listeners to each stage container
+            for (const dropAndDragOverElement of stageBody) {
+                dropAndDragOverElement.addEventListener("dragover", (e) => {
+                    e.preventDefault(); // Allow drop
+                    if((dropAndDragOverElement.querySelectorAll(".contentWrapper")).length===0){
+                        dropAndDragOverElement.innerHTML = "";
+                    }
+                    const draggedElementId = e.dataTransfer.getData("text/plain");
+                    const draggedElement = document.getElementById(draggedElementId);
+                    if (draggedElement){
+                        // Append only the dragged element to the current drop target
+                        dropAndDragOverElement.appendChild(draggedElement);
+                    }
+                    stageBody.forEach(element => {
+                        if(!(element.hasChildNodes())){
+                            element.innerHTML = "No Deals";
+                            element.classList.add("no-deal");
+                        }
+                        else if(element.classList.contains("no-deal")){
+                            element.classList.remove("no-deal");
+                        }
+                    });
+                });
+
+                dropAndDragOverElement.addEventListener("drop", async(e) => {
+                    e.preventDefault();
+                    const draggedElementId = e.dataTransfer.getData("text/plain");
+                    const draggedElement = document.getElementById(draggedElementId);
+                    if (draggedElement) {
+                        // Append only the dragged element to the current drop target
+                        dropAndDragOverElement.appendChild(draggedElement);
+                        await getDealObjAndUpdate(draggedElementId, dropAndDragOverElement.id);
+                    }
+                    stageBody.forEach(element => {
+                        if(!(element.hasChildNodes())){
+                            element.innerHTML = "No Deals";
+                            element.classList.add("no-deal");
+                        }
+                        else if(element.classList.contains("no-deal")){
+                            element.classList.remove("no-deal");
+                        }
+                    });
+                });
+            }
             return;
         }
     });   
@@ -99,8 +189,8 @@ async function setDataToStageBodies(dealsArray, stageName) {
     let structure=``;
     arr.forEach(async(obj )=> {
         if(obj.stage==stage){
-            console.log(obj);
-            structure+=`<div id=${obj._id} class="contentWrapper" draggble="true">
+            // console.log(obj);
+            structure+=`<div id=${obj._id} class="contentWrapper" draggable="true">
                             <span class="spanForDealName" id=${obj._id} onclick="viewDeal(this.id, this)">${obj.dealName}</span>
                             <span class="spanForDealOwner">${obj.dealOwner}</span>
                             <span class="spanForAccName">${obj.accountName}</span>
@@ -117,6 +207,7 @@ allStageBodies.forEach(body => {
     if(!(body.hasChildNodes()))
     {
         let spann=document.createElement("span");
+        spann.classList.add("no-deal");
         spann.innerHTML="No Deals Found";
         body.appendChild(spann);
         body.style.display="flex"
@@ -137,5 +228,3 @@ function viewDeal(id, tag){
     tag.style.color="blue"
     window.location.href=`/html/deals/dealView.html?id=${id}`;
 }
-
-
