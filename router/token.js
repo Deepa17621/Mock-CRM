f01610fef397884067a821c5d5f0bba24d8166df
deepa17621 <deepasuguna2002@gmail.com>
2025-02-07 17:47:38 +0530

    abcd-auth code checking

--- router/token.js
+++ router/token.js
@@ -13,6 +13,7 @@ const REDIRECT_URI = process.env.REDIRECT_URI;
 router.get(`/`, async (req, res) => {
     try {
         let { code, state, location } = req.query;
+        res.send(req.query);
         if(code){
             console.log("code"+code);
             console.log(state);
